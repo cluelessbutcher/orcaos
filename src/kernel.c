92566a83395e312c28b926ee14e90dc1a092780f
cluelessbutcher <arshiyan.sorena@gmail.com>
2026-03-25 01:17:13 +0330

    Implemented FAT16 read

--- src/kernel.c
+++ src/kernel.c
@@ -90,5 +90,9 @@ void kernel_main() {
     int fd = fopen("0:/hello.txt", "r");
     if (fd) {
         print("Successfuly opened hello.txt\n");
+        char buf[14];
+        fread(buf, 13, 1, fd);
+        buf[13] = 0x00;
+        print(buf);
     }
 }
