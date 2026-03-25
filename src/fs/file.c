48af569692e5e4a3fdf6f1ddb2174ceed3cc244e
cluelessbutcher <arshiyan.sorena@gmail.com>
2026-03-25 14:29:05 +0330

    Implemented fstat and fclose for fat16

--- src/fs/file.c
+++ src/fs/file.c
@@ -48,6 +48,11 @@ void fs_init() {
     fs_load();
 }
 
+static void file_free_descriptor(struct file_descriptor* desc) {
+    file_descriptors[desc->index-1] = 0x00;
+    kfree(desc);
+}
+
 static int file_new_descriptor(struct file_descriptor** desc_out) {
     int res = -ENOMEM;
     for (int i = 0; i < ORCAOS_MAX_FILE_DESCRIPTORS; i++) {
@@ -149,6 +154,37 @@ out:
         res = 0;
     }
 
+    return res;
+}
+
+int fstat(int fd, struct file_stat* stat) {
+    int res = 0;
+    struct file_descriptor* desc = file_get_descriptor(fd);
+    if (!desc) {
+        res = -EIO;
+        goto out;
+    }
+
+    res = desc->filesystem->stat(desc->disk, desc->private, stat);
+
+out:
+    return res;
+}
+
+int fclose(int fd) {
+    int res = 0;
+    struct file_descriptor* desc = file_get_descriptor(fd);
+    if (!desc) {
+        res = -EIO;
+        goto out;
+    }
+
+    res = desc->filesystem->close(desc->private);
+    if (res == ORCAOS_ALL_OK) {
+        file_free_descriptor(desc);
+    }
+        
+out:
     return res;
 }
 
