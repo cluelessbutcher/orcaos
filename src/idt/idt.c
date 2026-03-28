eec467d8060921882eee8744c4eb1987e15152de
cluelessbutcher <arshiyan.sorena@gmail.com>
2026-03-28 21:07:44 +0330

    Implemented a better way to call interrupts

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -4,10 +4,12 @@
 #include "kernel.h"
 #include "io/io.h"
 #include "task/task.h"
+#include "status.h"
 
 struct idt_desc idt_descriptors[ORCAOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
+static INTERRUPT_CALLBACK_FUNCTION interrupt_callbacks[ORCAOS_TOTAL_INTERRUPTS];
 extern void* interrupt_pointer_table[ORCAOS_TOTAL_INTERRUPTS];
 static ISR80H_COMMAND isr80h_commands[ORCAOS_MAX_ISR80H_COMMANDS];
 extern void idt_load(struct idtr_desc* ptr);
@@ -18,7 +20,13 @@ void no_interrupt_handler() {
     outb(0x20, 0x20);
 }
 
-void interrupt_handler(int interrupt, struct interrupt_frame* interrupt_frame) {
+void interrupt_handler(int interrupt, struct interrupt_frame* frame) {
+    kernel_page();
+    if (interrupt_callbacks[interrupt] != 0) {
+        task_current_save_state(frame);
+        interrupt_callbacks[interrupt](frame);
+    }
+    task_page();
     outb(0x20, 0x20);
 }
 
@@ -47,6 +55,16 @@ void idt_init() {
 	idt_set(0, idt_zero);		
     idt_set(0x80, isr80h_wrapper);
 	idt_load(&idtr_descriptor);
+}
+
+int idt_register_interrupt_callback(int interrupt, INTERRUPT_CALLBACK_FUNCTION interrupt_callback) {
+    if (interrupt < 0 || interrupt >=  ORCAOS_TOTAL_INTERRUPTS) {
+        return -EINVARG;
+    }
+
+    interrupt_callbacks[interrupt] = interrupt_callback;
+
+    return 0;
 }
 
 void isr80h_register_command(int command_id, ISR80H_COMMAND command) {
