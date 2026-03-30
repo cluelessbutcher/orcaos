039e9b42bc2c72f1dd09fe0b665c7ed995e64cd8
cluelessbutcher <arshiyan.sorena@gmail.com>
2026-03-30 16:22:24 +0330

    Implemented getkey in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -2,6 +2,10 @@
 
 int main(int argc, char** argv) {
     print("i fucking hate niggers they're black and they smell like shit\n");
-    while (1) {}
+    while (1) {
+        if (getkey() != 0) {
+            print("Key was pressed");
+        }
+    }
     return 0;
 }
